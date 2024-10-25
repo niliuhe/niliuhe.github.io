e101cf9ea54dd8c09f7bf07d45cb28ce83a2edd9
yibajianghudao <w2027075535@outlook.com>
2024-10-25 12:32:31 +0800

    update

--- script.js
+++ script.js
@@ -1,41 +1,33 @@
-// // script.js
-// console.log("This code runs on a schedule!");
-// // 你可以添加其他功能，比如 API 请求等
-// async function fetchSheetData() {
-//     const sheetId = '1cG4Tlm1YgdLCZKuivkSyy8pOcx-YzEZ5Trxh8GYvhDY';
-//     const apiKey = 'AIzaSyAKMN26phfEvXQKd9o-za_hpzHlptGzBFA';
-//     const range = 'Sheet1';
-    
-//     const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`);
-//     const data = await response.json();
-    
-//     if (data.values) {
-//         console.log('Data:', data);
-//         console.log('Data:', data.values);
-//     } else {
-//         console.log('No data found.');
-//     }
-// }
-// fetchSheetData();
-
 const { google } = require('googleapis');
-console.log("131");
+const fs = require('fs');
+
+
 async function getSheetValues(auth) {
   const sheets = google.sheets({ version: 'v4', auth });
   const spreadsheetId = '1cG4Tlm1YgdLCZKuivkSyy8pOcx-YzEZ5Trxh8GYvhDY'; // 替换为你的表格ID
-  const range = 'Sheet1!A1:D5';
+  const range = '工作表1';
+  const apiKey = 'AIzaSyAKMN26phfEvXQKd9o-za_hpzHlptGzBFA';
 
   try {
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId,
       range,
+      key:apiKey,
     });
-    console.log("11111");
-    console.log(response.data);
+    // console.log(response.data.values);
+    const data = response.data.values;
+    const jsonData = Object.fromEntries(data.slice(1));
+    fs.writeFile('config.json', JSON.stringify(jsonData, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing to config.json:', err);
+      } else {
+        console.log('config.json has been created successfully!');
+      }
+    });
   } catch (error) {
     console.error('Error fetching data from Google Sheets:', error);
   }
 }
 
 // 认证部分省略
-
+getSheetValues();
